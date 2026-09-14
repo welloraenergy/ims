88773cbc11fe83d0bbcc1d4112e96c03f2a4ff8d
noagrp <lihwlee11@gmail.com>
2026-09-14 16:02:21 +0800

    Refresh automatic PO expected quantity module

--- ims-app.js
+++ ims-app.js
@@ -2,5 +2,5 @@ import './ims-permissions.js';
 import './ims-shell.js';
 import './ims-access-ui.js';
 import './po-history-metrics.js?v=20260914-44';
-import './ims-po-auto-expected.js?v=20260914-45';
+import './ims-po-auto-expected.js?v=20260914-46';
 import './ims-bootstrap.js?v=20260914-43';
