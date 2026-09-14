457b12874d03d2dd06d7b00d01ae949176823a8d
noagrp <lihwlee11@gmail.com>
2026-09-14 16:14:35 +0800

    Use PO lifecycle metrics in Documents

--- ims-app.js
+++ ims-app.js
@@ -1,6 +1,5 @@
 import './ims-permissions.js';
 import './ims-shell.js';
 import './ims-access-ui.js';
-import './po-history-metrics.js?v=20260914-44';
-import './ims-po-auto-expected.js?v=20260914-46';
+import './po-lifecycle-metrics.js?v=20260914-47';
 import './ims-bootstrap.js?v=20260914-43';
