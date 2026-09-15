8d3870a518f7d5c4d5932522296183e052627d65
noagrp <lihwlee11@gmail.com>
2026-09-15 17:07:13 +0800

    Bump IMS build for Item Directory size filter

--- ims-module-loader.js
+++ ims-module-loader.js
@@ -1,6 +1,6 @@
-import { can, currentRole } from './ims-permissions.js?v=20260915-58';
+import { can, currentRole } from './ims-permissions.js?v=20260915-59';
 
-const IMS_BUILD='20260915-58';
+const IMS_BUILD='20260915-59';
 const versioned=src=>`${src}${src.includes('?')?'&':'?'}v=${IMS_BUILD}`;
 
 const MODULES=Object.freeze([
