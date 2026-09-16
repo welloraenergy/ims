075dd84d13eca061c90ee11a3ce4048a57bfc4b5
noagrp <lihwlee11@gmail.com>
2026-09-16 23:58:53 +0800

    Bump IMS build for navigation design cleanup

--- ims-module-loader.js
+++ ims-module-loader.js
@@ -1,6 +1,6 @@
-import { can, currentRole } from './ims-permissions.js?v=20260916-73';
+import { can, currentRole } from './ims-permissions.js?v=20260916-74';
 
-const IMS_BUILD='20260916-73';
+const IMS_BUILD='20260916-74';
 const versioned=src=>`${src}${src.includes('?')?'&':'?'}v=${IMS_BUILD}`;
 
 const MODULES=Object.freeze([
