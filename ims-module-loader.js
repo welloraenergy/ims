1e439b6c82a7d7578765c09256cf9514386b3fc5
noagrp <lihwlee11@gmail.com>
2026-09-16 22:00:11 +0800

    Bump IMS build 67 after reversal delete fix

--- ims-module-loader.js
+++ ims-module-loader.js
@@ -1,6 +1,6 @@
-import { can, currentRole } from './ims-permissions.js?v=20260916-66';
+import { can, currentRole } from './ims-permissions.js?v=20260916-67';
 
-const IMS_BUILD='20260916-66';
+const IMS_BUILD='20260916-67';
 const versioned=src=>`${src}${src.includes('?')?'&':'?'}v=${IMS_BUILD}`;
 
 const MODULES=Object.freeze([
