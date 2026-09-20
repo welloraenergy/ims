7d40fdc4f78a151e16ca319ba0b513c43a57bac3
noagrp <lihwlee11@gmail.com>
2026-09-21 02:03:07 +0800

    Bump IMS build for unified picker visibility

--- ims-module-loader.js
+++ ims-module-loader.js
@@ -1,6 +1,6 @@
-import { can, currentRole } from './ims-permissions.js?v=20260920-80';
+import { can, currentRole } from './ims-permissions.js?v=20260920-81';
 
-const IMS_BUILD='20260920-80';
+const IMS_BUILD='20260920-81';
 const versioned=src=>`${src}${src.includes('?')?'&':'?'}v=${IMS_BUILD}`;
 
 const MODULES=Object.freeze([
