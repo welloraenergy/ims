fac9ffc4debdabadabea3fad5560f0d24408626c
noagrp <lihwlee11@gmail.com>
2026-09-21 01:58:06 +0800

    Bump IMS build after Movement picker cleanup

--- ims-module-loader.js
+++ ims-module-loader.js
@@ -1,6 +1,6 @@
-import { can, currentRole } from './ims-permissions.js?v=20260920-79';
+import { can, currentRole } from './ims-permissions.js?v=20260920-80';
 
-const IMS_BUILD='20260920-79';
+const IMS_BUILD='20260920-80';
 const versioned=src=>`${src}${src.includes('?')?'&':'?'}v=${IMS_BUILD}`;
 
 const MODULES=Object.freeze([
