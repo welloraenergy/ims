f5d0ad9ef9a73471f645496907412d4923c11f00
noagrp <lihwlee11@gmail.com>
2026-09-21 03:10:22 +0800

    Bump IMS build for Unit Price registration schema

--- ims-module-loader.js
+++ ims-module-loader.js
@@ -1,6 +1,6 @@
-import { can, currentRole } from './ims-permissions.js?v=20260920-85';
+import { can, currentRole } from './ims-permissions.js?v=20260920-86';
 
-const IMS_BUILD='20260920-85';
+const IMS_BUILD='20260920-86';
 const versioned=src=>`${src}${src.includes('?')?'&':'?'}v=${IMS_BUILD}`;
 
 const MODULES=Object.freeze([
