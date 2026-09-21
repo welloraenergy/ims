d5fcf23fa8644ecab128bab16f0270b48914d1e9
noagrp <lihwlee11@gmail.com>
2026-09-21 09:31:24 +0800

    Bump IMS build for currency amount display

--- ims-module-loader.js
+++ ims-module-loader.js
@@ -1,6 +1,6 @@
-import { can, currentRole } from './ims-permissions.js?v=20260920-89';
+import { can, currentRole } from './ims-permissions.js?v=20260920-90';
 
-const IMS_BUILD='20260920-89';
+const IMS_BUILD='20260920-90';
 const versioned=src=>`${src}${src.includes('?')?'&':'?'}v=${IMS_BUILD}`;
 
 const MODULES=Object.freeze([
