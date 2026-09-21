79885a13fdc705432e194e4c07ff2c1b32073082
noagrp <lihwlee11@gmail.com>
2026-09-21 08:07:52 +0800

    Bump IMS build after backup and input policy hardening

--- ims-module-loader.js
+++ ims-module-loader.js
@@ -1,6 +1,6 @@
-import { can, currentRole } from './ims-permissions.js?v=20260920-87';
+import { can, currentRole } from './ims-permissions.js?v=20260920-88';
 
-const IMS_BUILD='20260920-87';
+const IMS_BUILD='20260920-88';
 const versioned=src=>`${src}${src.includes('?')?'&':'?'}v=${IMS_BUILD}`;
 
 const MODULES=Object.freeze([
